79116bfc722761458d4e4ea477701670960b9d4f
Mohammad Javad <mohammadjavad.mousavi97@gmai.com>
2025-02-23 20:52:59 +0330

    update Q2.m file for second time

--- HW1/Q2.m
+++ HW1/Q2.m
@@ -18,20 +18,14 @@ imshow(skull_img);
 title('Original Image');
 
 % Define a function to reduce intensity levels
-reduce_level = @(img, levels) uint8(floor(double(img) / (256 / levels)) * (256 / levels));
+reduce_level = @(img, levels) uint8(floor(double(img) / (256 / levels)) * (256 / (levels - 1)));
 
-% Function for reducing to 2 intensity levels (black and white)
-reduce_level_2 = @(img) uint8(img > 127) * 255;  % Pixels are divided into two groups: 0 or 255
-
-levels = [64, 16, 4, 2];
+levels = [64, 16, 8, 4, 2];
 
 for i = 1:length(levels)
-    if levels(i) == 2
-        img_reduced = reduce_level_2(skull_img);
-    else
-        img_reduced = reduce_level(skull_img, levels(i));
-    end
     
+    img_reduced = reduce_level(skull_img, levels(i));
+
     % Display images with different intensity levels
     subplot(2, 3, i + 1);
     imshow(img_reduced);
