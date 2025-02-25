fc9f7eff5e141f3b345a302d28cb819a7a148c90
Mohammad Javad <mohammadjavad.mousavi97@gmai.com>
2025-02-25 21:07:30 +0330

    The first completed version of Q4.m file with all functions that was used

--- HW1/Q4.m
+++ HW1/Q4.m
@@ -25,14 +25,116 @@ rect_height2 = 90;
 array2(x_start2:x_start2+rect_height2-1, y_start2:y_start2+rect_length2-1) = 1;
 
 % Display the arrays to verify the rectangle placement
-figure;
+figure(1);
 subplot(2,2,1);
 imshow(array1);
-title('Array 1 with Rectangle');
+title('Array 1');
+
 subplot(2,2,2);
 imshow(array2);
-title('Array 2 with Rectangle');
+title('Array 2');
 
-%%
+image_not_array1 = logical_not(array1);
+image_not_array2 = logical_not(array2);
+
 subplot(2,2,3);
-imshow(logical_not(array2));
+imshow(image_not_array1);
+title('Not array 1');
+
+subplot(2,2,4);
+imshow(image_not_array2);
+title('Not array 2');
+
+%% AND
+% Find the border of the rectangles (the edges of the 1's in the arrays)
+
+image_and = logical_and(array1, array2);
+
+% For array1
+image_and(x_start1,y_start1:y_start1+rect_length1-1) = 1;
+image_and(x_start1+rect_height1-1,y_start1:y_start1+rect_length1-1) = 1;
+image_and(x_start1:x_start1+rect_height1-1, y_start1) = 1;
+image_and(x_start1:x_start1+rect_height1-1, y_start1+rect_length1-1) = 1;
+
+% For array2
+image_and(x_start2,y_start2:y_start2+rect_length2-1) = 1;
+image_and(x_start2+rect_height2-1,y_start2:y_start2+rect_length2-1) = 1;
+image_and(x_start2:x_start2+rect_height2-1, y_start2) = 1;
+image_and(x_start2:x_start2+rect_height2-1, y_start2+rect_length2-1) = 1;
+
+figure(2);
+subplot(1,3,1);
+imshow(array1);
+title('Array 1');
+subplot(1,3,2);
+imshow(array2);
+title('Array 2');
+subplot(1,3,3);
+imshow(image_and);
+title('Array 1 AND Array 2');
+
+%% OR
+
+image_or = logical_or(array1, array2);
+
+figure(3);
+subplot(1,3,1);
+imshow(array1);
+title('Array 1');
+
+subplot(1,3,2);
+imshow(array2);
+title('Array 2');
+
+subplot(1,3,3);
+imshow(image_or);
+title('Array 1 OR Array 2');
+
+%% AND-NOT
+
+image_and_not = logical_and_not(array1, array2);
+
+figure(4);
+subplot(1,3,1);
+imshow(array1);
+title('Array 1');
+
+subplot(1,3,2);
+imshow(array2);
+title('Array 2');
+
+subplot(1,3,3);
+
+% For array2
+image_and_not(x_start2,y_start2:y_start2+rect_length2-1) = 1;
+image_and_not(x_start2+rect_height2-1,y_start2:y_start2+rect_length2-1) = 1;
+image_and_not(x_start2:x_start2+rect_height2-1, y_start2) = 1;
+image_and_not(x_start2:x_start2+rect_height2-1, y_start2+rect_length2-1) = 1;
+
+imshow(image_and_not);
+title('Array 1 AND [NOT Array 2]');
+
+%% XOR
+
+figure(5);
+image_xor = manual_xor(array1, array2);
+
+subplot(1,3,1);
+imshow(array1);
+title('Array 1');
+
+subplot(1,3,2);
+imshow(array2);
+title('Array 2');
+
+subplot(1,3,3);
+imshow(image_xor);
+title('Array 1 XOR Array 2');
+
+
+
+
+
+
+
+
---
+++ HW1/logical_and.m
@@ -0,0 +1,10 @@
+% AND operation
+function result = logical_and(A, B)
+    [rows, cols] = size(A);
+    result = zeros(rows, cols);  
+    for x = 1:rows
+        for y = 1:cols
+            result(x, y) = A(x, y) & B(x, y);  
+        end
+    end
+end
